17facc237a7bd1fb9e61efe518faa9ad638f1198
Ruben d'Arco <cyclops+github@prof-x.net>
2016-02-01 23:30:54 +0000

    Some improvements to the commands

--- biltongbox.py
+++ biltongbox.py
@@ -23,9 +23,6 @@ gpio_fan1 = 22
 gpio_allneutral = 23
 gpio_pwm_fan1 = 18
 
-# Target temp
-target_temp = 30
-
 def htu_reset():
 	handle = pi.i2c_open(bus, addr) 
 	pi.i2c_write_byte(handle, 0xFE) 
@@ -72,9 +69,12 @@ def Fan1(speed):
 	
 	if (speed < 10):
 		pi.write(gpio_fan1, 0)
+		print "[Fan1] Turning off."
 	else:
 		pi.write(gpio_fan1, 1)
 		pi.hardware_PWM(gpio_pwm_fan1, 25000, speed*10000)
+		print "[Fan1] Setting to", speed,"%"
+
 def FanSpeed1():
 	if (IsNeutral() == False or pi.read(gpio_fan1) == 0):
 		return 0
@@ -90,8 +90,10 @@ def Heating(onOff):
 
 	if (onOff == True):
 		pi.write(gpio_heating1, 1)
+		print "[Heat] Turning on"
 	else:
 		pi.write(gpio_heating1, 0)
+		print "[Heat] Turning off"
 
 
 def IsHeating():
@@ -113,11 +115,16 @@ def startup():
 
 
 def makeBiltong(cmds):
+	# Targets
+	target_temp = 21
+	fan1_heatspeed=70
+	fan1_nonheatspeed=10
+
+
 	i = 0
 	while True:
 		if (i > 10):
 			(temp, hum) = read_environment()
-			print "Current temperateure/humid:", temp,"/",hum
 			now = datetime.datetime.today()	
 			line = "%s,%s,%s\n" %(now, temp, hum)
 			with open("test.txt", "a") as myfile:
@@ -125,12 +132,10 @@ def makeBiltong(cmds):
 
 			if (temp < target_temp and IsHeating() == False):
 				Heating(True)
-				Fan1(50)
-				print "Heating On, Fan to 50%"
+				Fan1(fan1_heatspeed)
 			if (temp >= (target_temp+0.5) and IsHeating() == True):
 				Heating(False)
-				Fan1(10)
-				print "Heating Off, Fan to 10%"
+				Fan1(fan1_nonheatspeed)
 			i=0
 		
 		if (cmds.empty() == False):
@@ -142,7 +147,25 @@ def makeBiltong(cmds):
 				Neutral(False)
 				break
 
-			# More command here
+			if line.startswith('temp') == True:
+				target_temp = int(line[5:])
+				print "Setting target temp to ", target_temp
+			
+
+			if line.startswith('fanspeedon') == True:
+				fan1_heatspeed = int(line[11:])
+				print "Setting heating on fan speed to ", fan1_heatspeed
+
+			if line.startswith('fanspeedoff') == True:
+				fan1_nonheatspeed = int(line[12:])
+				print "Setting heating on fan speed to ", fan1_nonheatspeed
+
+			if line == 'status':
+				(temp, hum) = read_environment()
+				print "Current temperateure/humid:", temp,"/",hum
+				print "Heating is:", IsHeating()
+				print "FanSpeed  :", FanSpeed1()
+
 		time.sleep(1)
 		i = i+1
 

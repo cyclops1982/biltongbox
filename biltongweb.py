0764e7b8e74c7b028c0054ee46c77697cdae39e1
Ruben d'Arco <cyclops-github@prof-x.net>
2016-04-24 08:04:08 +0100

    web.py try

--- biltongweb.py
+++ biltongweb.py
@@ -2,18 +2,41 @@
 # Simple script to control temperateu based on heat sensor reading #
 ####################################################################
 import web
+import mimetypes
+import sys
 
+render = web.template.render('html/')
 
-render=web.template.render('html/')
+static_dirs = ('css', 'js', 'images')
 
 urls = (
-	'/(.*)', 'index'
+	'/', 'index',
+	'/(css|js|images)/.*', 'static'
 )
 
 
+
+class static:
+	def mime_type(self, filename):
+		return mimetypes.guess_type(filename)[0] or 'application/octet-stream'
+
+	def GET(self, static_dir):
+		try:
+			fullpath = './html'
+			fullpath += web.ctx.path
+			mimetype = self.mime_type(fullpath)
+			web.header('Content-type', mimetype)
+			with open(fullpath, 'rb') as readfile:
+				return readfile.read() 
+		except IOError:
+			web.notfound()
+
+
+
 class index:
-	def GET(self, name):
-		return render.index(name)
+	def GET(self):
+		return render.index('test')
+
 
 
 if __name__ == "__main__":
